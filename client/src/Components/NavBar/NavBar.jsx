8787695e9848c60dec1e6df066bd56b87588a1f6
vraktos <matidemonte@hotmail.com>
2023-03-21 14:51:03 -0300

    refactor for deploy

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,39 +1,20 @@
 import style from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className={style.navContainer}>
-      <button
-        className={style.button}
-        onClick={(e) => {
-          e.preventDefault();
-          window.location.href =
-            "https://countries-pi-production-c7b2.up.railway.app";
-        }}
-      >
+      <button className={style.button}>
         {" "}
+        <Link to="https://countries-pi-production-c7b2.up.railway.app"></Link>{" "}
         Landing{" "}
       </button>{" "}
-      <button
-        className={style.button}
-        onClick={(e) => {
-          e.preventDefault();
-          window.location.href =
-            "https://countries-pi-production-c7b2.up.railway.app/home";
-        }}
-      >
-        {" "}
+      <button className={style.button}>
+        <Link to="https://countries-pi-production-c7b2.up.railway.app/home"></Link>{" "}
         Home{" "}
       </button>{" "}
-      <button
-        className={style.button}
-        onClick={(e) => {
-          e.preventDefault();
-          window.location.href =
-            "https://countries-pi-production-c7b2.up.railway.app/create";
-        }}
-      >
-        {" "}
+      <button className={style.button}>
+        <Link to="https://countries-pi-production-c7b2.up.railway.app/create"></Link>{" "}
         Create your Adventure!{" "}
       </button>{" "}
     </div>
